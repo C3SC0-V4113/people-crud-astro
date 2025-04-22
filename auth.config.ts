bd8b5f36542eef5e0fefaab52e87b17927473ecb
Francisco Valle <cescovalledev@gmail.com>
2025-04-22 15:50:26 -0600

    credentials form added

--- auth.config.ts
+++ auth.config.ts
@@ -1,3 +1,4 @@
+import Credentials from "@auth/core/providers/credentials";
 import Google from "@auth/core/providers/google";
 import { defineConfig } from "auth-astro";
 
@@ -7,5 +8,32 @@ export default defineConfig({
       clientId: import.meta.env.GOOGLE_CLIENT_ID,
       clientSecret: import.meta.env.GOOGLE_CLIENT_SECRET,
     }),
+    Credentials({
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        console.log(credentials);
+        if (credentials.email === "frankjose00@gmail.com") return null; // Invalid credentials
+
+        return {
+          id: "1",
+          name: "John Doe",
+          email: credentials?.email as string,
+        };
+      },
+    }),
   ],
+  events: {
+    async signIn({ user, account, profile, isNewUser }) {
+      console.log("Usuario Logueado", user);
+    },
+  },
+  pages: {
+    signIn: "/login",
+    signOut: "/login",
+    error: "/login", // Error code passed in query string as ?error=
+    verifyRequest: "/verify-request", // (used for check email message)
+  },
 });
---
+++ src/components/auth/login/credentials-form.tsx
@@ -0,0 +1,89 @@
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { signIn } from "auth-astro/client";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+
+const formSchema = z.object({
+  email: z.string().min(2).max(50).email("Invalid email"),
+  password: z.string().min(8).max(50),
+});
+
+export const CredentialsForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  const [authError, setAuthError] = useState<string>();
+
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    await signIn("credentials", {
+      prefix: "/api/auth",
+      ...data,
+    });
+  };
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const error = params.get("error");
+
+    if (!error) return;
+    console.error("Error de login:", error);
+    setAuthError(error?.toString());
+  }, []);
+
+  useEffect(() => {
+    if (!authError) return;
+    toast.error(authError);
+  }, [authError]);
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="email@people.com" type="email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input placeholder="********" type="password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button className="w-full" type="submit">
+          Log In with Email
+        </Button>
+      </form>
+    </Form>
+  );
+};

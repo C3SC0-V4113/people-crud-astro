5086ad1312a764a3327940ac7b670fb4ad42ad61
Francisco Valle <cescovalledev@gmail.com>
2025-04-23 17:07:21 -0600

    advanced create user form

--- src/components/auth/register/create-user-form.tsx
+++ src/components/auth/register/create-user-form.tsx
@@ -1,3 +1,148 @@
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { isValidPhoneNumber } from "react-phone-number-input";
+import { PhoneInput } from "@/components/ui/phone-input";
+import { api } from "@/lib/clients";
+import { toast } from "sonner";
+import { isAxiosError } from "axios";
+
+const formSchema = z
+  .object({
+    email: z.string().min(2).max(50).email("Invalid email"),
+    password: z.string().min(8).max(50),
+    repeat_password: z.string().min(8).max(50),
+    name: z.string().min(2).max(50),
+    image: z.string().optional(),
+    phone: z
+      .string()
+      .refine(isValidPhoneNumber, { message: "Invalid phone number" }),
+  })
+  .refine((data) => data.password === data.repeat_password, {
+    message: "Las contraseñas no coinciden",
+    path: ["repeat_password"],
+  });
+
 export const CreateUserForm = () => {
-  return <div>CreateUserForm</div>;
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      repeat_password: "",
+      image: "",
+      name: "",
+      phone: "",
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    console.log(data);
+    try {
+      const resp = await api.post("/user/register", {
+        name: data.name,
+        email: data.email,
+        password: data.password,
+      });
+
+      if (resp.status !== 200) {
+        throw new Error(resp.data.message);
+      }
+      console.log("Usuario creado:", resp.data);
+      form.reset();
+    } catch (error) {
+      toast.error(
+        error
+          ? isAxiosError(error)
+            ? error?.response?.data?.message
+            : error
+          : "Error desconocido"
+      );
+    }
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input placeholder="Name" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="email@people.com" type="email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input placeholder="********" type="password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="repeat_password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Repeat Password</FormLabel>
+              <FormControl>
+                <Input placeholder="********" type="password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="phone"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Phone number</FormLabel>
+              <FormControl>
+                <PhoneInput placeholder="+50377777777" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button className="w-full" type="submit">
+          Create your account
+        </Button>
+      </form>
+    </Form>
+  );
 };
